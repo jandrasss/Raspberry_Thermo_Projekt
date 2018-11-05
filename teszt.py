a18049f9c46ea203b9de4e2f7027233a634f360d
jandrasss <jandrasss@users.noreply.github.com>
2018-11-05 19:31:33 +0100

    Csak

--- teszt.py
+++ teszt.py
@@ -1,6 +1,25 @@
-def kiir(szoveg):
-    print(szoveg)
+from _thread import *
+import time
+# Define a function for the thread
 
 
-kiir("Szia")
-kiir("Hello")
+def print_time(threadName, delay):
+    count = 0
+    while count < 5:
+        time.sleep(delay)
+        count += 1
+        print ("%s: %s" % ( threadName, time.ctime(time.time()) ))
+# Create two threads as follows
+
+
+try:
+    start_new_thread( print_time, ("Thread-1", 2, ) )
+    start_new_thread( print_time, ("Thread-2", 4, ) )
+
+
+except:
+    print('Error: unable to start thread')
+
+
+while 1:
+    pass
