ded57ff81eaeb2bcc27ed27bd5a9f0d29d090589
jandrasss <jandrasss@users.noreply.github.com>
2018-11-25 00:59:14 +0100

    .

--- prac.py
+++ prac.py
@@ -15,7 +15,7 @@ class TemperatureSensors:
         #self.precision = 10
         self.sensor = W1ThermSensor(W1ThermSensor.THERM_SENSOR_DS18B20, config['sysbus'][3:])
         self.temp = 0
-        thread = threading.Thread(target=self.run, args==())
+        thread = threading.Thread(target=self.run, args=())
         thread.daemon = True
         thread.start()
 
